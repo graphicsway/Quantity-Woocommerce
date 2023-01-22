95b2e3ec67ae4aad0367cf91689f333e4358dad3
Graphics Way <98776063+graphicsway@users.noreply.github.com>
2023-01-22 17:39:18 +0300

    Add Quantity button for the Shopping Page.

---
+++ quantity.php
@@ -0,0 +1,10 @@
+function quantity_for_woocommerce_loop( $html, $product ) {
+    if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
+        $html = '<form action="' . esc_url( $product->add_to_cart_url() ) . '" class="cart" method="post" enctype="multipart/form-data">';
+        $html .= woocommerce_quantity_input( array(), $product, false );
+        $html .= '<button type="submit" class="button alt">' . esc_html( $product->add_to_cart_text() ) . '</button>';
+        $html .= '</form>';
+    }
+    return $html;
+}
+add_filter( 'woocommerce_loop_add_to_cart_link', 'quantity_for_woocommerce_loop', 10, 2 );
